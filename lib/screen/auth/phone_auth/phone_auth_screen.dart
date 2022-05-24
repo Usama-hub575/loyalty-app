119cd83e1a03898d517f64f4a5c44aa52d9c1999
$ faizanch0305 <faizanch0305@gmail.com>
2022-05-24 21:30:36 +0500

    wip

--- lib/screen/auth/phone_auth/phone_auth_screen.dart
+++ lib/screen/auth/phone_auth/phone_auth_screen.dart
@@ -19,21 +19,21 @@ class PhoneAuthScreen extends GetView<PhoneAuthController> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              verticalSpacer(100),
+              verticalSpacer(83),
               SvgPicture.asset(
                 assets.icAactivpay,
                 width: sizes.widthRatio * 136,
               ),
-              verticalSpacer(28),
+              verticalSpacer(12),
               HeadingLargeText(constants.getStarted),
               verticalSpacer(10),
               BodyLargeText(
                 constants.continueWithYourPhoneNumber,
                 color: colors.primaryDark,
               ),
-              verticalSpacer(40),
+              verticalSpacer(70),
               NumberField(),
-              verticalSpacer(40),
+              verticalSpacer(50),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [

cbe52c078c06cca4c36d7f98b4b02832965f2b4d
faran13 <muhammadfaran669@gmail.com>
2022-05-24 23:17:34 +0500

    update code

--- lib/screen/auth/phone_auth/otp_verification/widgets/pin_code_field.dart
+++ lib/screen/auth/phone_auth/otp_verification/widgets/pin_code_field.dart
@@ -17,63 +17,70 @@ class PinCodeField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Form(
-      // key: formKey,
-      child: PinCodeTextField(
-        appContext: context,
-        controller: verificationCodeController,
-        pastedTextStyle: textStyles.bodyLarge,
-        length: 6,
-        obscureText: false,
-        // animationType: AnimationType.fade,
-        autoFocus: true,
-        enablePinAutofill: true,
-        pinTheme: PinTheme(
-          shape: PinCodeFieldShape.box,
-          borderRadius: BorderRadius.circular(8),
-          borderWidth: 0,
-          fieldHeight: 50,
-          fieldWidth: 40,
-          activeFillColor: colors.white,
-          inactiveFillColor: colors.white,
-          selectedFillColor: colors.white,
-          inactiveColor: colors.primaryDark.withOpacity(0.05),
-          selectedColor: controller.isFocus.value? colors.accentPrimary : colors.white,
-          activeColor: controller.isFocus.value? colors.accentPrimary : colors.white,
-        ),
-        hintCharacter: '-',
-        hintStyle: textStyles.bodyLarge,
-        cursorColor: colors.primaryDark,
-        animationType: AnimationType.fade,
-        textStyle: textStyles.bodyLarge,
-        enableActiveFill: true,
-        backgroundColor: colors.primaryLight,
-
-        keyboardType: TextInputType.number,
-
-        onCompleted: (v) {
-          print("Completed");
-
-          onComplete();
-        },
-        onTap: () {
-          print("Pressed");
-        },
-        onChanged: (value) {
-          validateOTP();
-        },
-
-            beforeTextPaste: (text) {
-              print("Allowing to paste $text");
-              //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
-              //but you can show anything you want here, like your pop up saying wrong paste format or etc
-              return true;
+      child: GetX<OTPVerificationController>(
+        builder: (controller) {
+          return Focus(
+            onFocusChange: (hasFocus) {
+              controller.isFocus.value = hasFocus;
             },
-          ),
-          onFocusChange: (hasFocus){
-            controller.isFocus.value = hasFocus;
-          },
-        );
-      },),
+            child: PinCodeTextField(
+              appContext: context,
+              controller: verificationCodeController,
+              pastedTextStyle: textStyles.bodyLarge,
+              length: 6,
+              obscureText: false,
+              // animationType: AnimationType.fade,
+              autoFocus: true,
+              enablePinAutofill: true,
+              pinTheme: PinTheme(
+                shape: PinCodeFieldShape.box,
+                borderRadius: BorderRadius.circular(8),
+                borderWidth: 0,
+                fieldHeight: 50,
+                fieldWidth: 40,
+                activeFillColor: colors.white,
+                inactiveFillColor: colors.white,
+                selectedFillColor: colors.white,
+                inactiveColor: colors.primaryDark.withOpacity(0.05),
+                selectedColor: controller.isFocus.value
+                    ? colors.accentPrimary
+                    : colors.white,
+                activeColor: controller.isFocus.value
+                    ? colors.accentPrimary
+                    : colors.white,
+              ),
+              hintCharacter: '-',
+              hintStyle: textStyles.bodyLarge,
+              cursorColor: colors.primaryDark,
+              animationType: AnimationType.fade,
+              textStyle: textStyles.bodyLarge,
+              enableActiveFill: true,
+              backgroundColor: colors.primaryLight,
+
+              keyboardType: TextInputType.number,
+
+              onCompleted: (v) {
+                print("Completed");
+
+                onComplete();
+              },
+              onTap: () {
+                print("Pressed");
+              },
+              onChanged: (value) {
+                validateOTP();
+              },
+
+              beforeTextPaste: (text) {
+                print("Allowing to paste $text");
+                //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                return true;
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/screen/auth/phone_auth/phone_auth_screen.dart
+++ lib/screen/auth/phone_auth/phone_auth_screen.dart
@@ -5,128 +5,139 @@ import 'package:get/get.dart';
 import 'package:aactivpay/export.dart';
 
 class PhoneAuthScreen extends GetView<PhoneAuthController> {
-
   @override
   Widget build(BuildContext context) {
     controller.initialize();
-    return Scaffold(
-      backgroundColor: colors.primaryLight,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            verticalSpacer(100),
-            SvgPicture.asset(
-              assets.icAactivpay,
-              width: sizes.widthRatio * 136,
-            ),
-            verticalSpacer(28),
-            HeadingLargeText(constants.getStarted),
-            verticalSpacer(10),
-            BodyLargeText(
-              constants.continueWithYourPhoneNumber,
-              color: colors.primaryDark,
-            ),
-            verticalSpacer(40),
-            NumberField(),
-            verticalSpacer(40),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 4),
-                  child: SizedBox(
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        backgroundColor: colors.primaryLight,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              verticalSpacer(83),
+              SvgPicture.asset(
+                assets.icAactivpay,
+                width: sizes.widthRatio * 136,
+              ),
+              verticalSpacer(12),
+              HeadingLargeText(constants.getStarted),
+              verticalSpacer(10),
+              BodyLargeText(
+                constants.continueWithYourPhoneNumber,
+                color: colors.primaryDark,
+              ),
+              verticalSpacer(70),
+              NumberField(),
+              verticalSpacer(50),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 4),
+                    child: SizedBox(
                       width: 16,
                       height: 16,
                       child: Obx(
-                        ()=> Checkbox(
+                        () => Checkbox(
                           value: controller.isCheck.value,
-                          onChanged: (value)=>controller.onTapCheckBox(value),
+                          onChanged: (value) => controller.onTapCheckBox(value),
                           checkColor: colors.accentPrimary,
-                          fillColor:
-                              MaterialStateProperty.all(Colors.transparent),
-                          side: MaterialStateBorderSide.resolveWith((_) =>
-                              BorderSide(
-                                  width: 1,
-                                  color: controller.isCheck.value
-                                      ? colors.accentPrimary
-                                      : colors.primaryDark)),
+                          fillColor: MaterialStateProperty.all(
+                            Colors.transparent,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(3),
+                            ),
+                          ),
+                          side: MaterialStateBorderSide.resolveWith(
+                            (_) => BorderSide(
+                              width: 2.5,
+                              color: controller.isCheck.value
+                                  ? colors.accentPrimary
+                                  : colors.primaryDark.withOpacity(0.2),
+                            ),
+                          ),
                         ),
-                      )),
-                ),
-                horizontalSpacer(10),
-                Expanded(
-                    child: RichText(
-                  text: TextSpan(
-                      text: constants.termAndConditionsText,
-                      style: textStyles.bodyExtraSmall.copyWith(
-                        color: colors.primaryDark,
                       ),
-                      children: [
-                        TextSpan(
-                          text: constants.termsAndCondition,
-                          style: textStyles.bodyExtraSmall.copyWith(
-                            color: colors.accentPrimary,
-                          ),
+                    ),
+                  ),
+                  horizontalSpacer(10),
+                  Expanded(
+                      child: RichText(
+                    text: TextSpan(
+                        text: constants.termAndConditionsText,
+                        style: textStyles.bodyExtraSmall.copyWith(
+                          color: colors.primaryDark,
                         ),
-                        WidgetSpan(
-                          child: GestureDetector(
-                            onTap: () {
-                              controller.navigateToTermsAndConditionsPage();
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 3, right: 3, bottom: 1),
-                              child: SvgPicture.asset(
-                                assets.icTermConditionLink,
-                                height: 15,
+                        children: [
+                          TextSpan(
+                            text: constants.termsAndCondition,
+                            style: textStyles.bodyExtraSmall.copyWith(
+                              color: colors.accentPrimary,
+                            ),
+                          ),
+                          WidgetSpan(
+                            child: GestureDetector(
+                              onTap: () {
+                                controller.navigateToTermsAndConditionsPage();
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 3, right: 3, bottom: 1),
+                                child: SvgPicture.asset(
+                                  assets.icTermConditionLink,
+                                  height: 10,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        TextSpan(
-                          text: " and ",
-                          style: textStyles.bodyExtraSmall.copyWith(
-                            color: colors.primaryDark,
+                          TextSpan(
+                            text: " and ",
+                            style: textStyles.bodyExtraSmall.copyWith(
+                              color: colors.primaryDark,
+                            ),
                           ),
-                        ),
-                        TextSpan(
-                          text: constants.privacyPolicy,
-                          style: textStyles.bodyExtraSmall.copyWith(
-                            color: colors.accentPrimary,
+                          TextSpan(
+                            text: constants.privacyPolicy,
+                            style: textStyles.bodyExtraSmall.copyWith(
+                              color: colors.accentPrimary,
+                            ),
                           ),
-                        ),
-                        WidgetSpan(
-                          child: GestureDetector(
-                            onTap: () {
-                              controller.navigateToTermsAndConditionsPage();
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 3, right: 3, bottom: 1),
-                              child: SvgPicture.asset(
-                                assets.icTermConditionLink,
-                                height: 15,
+                          WidgetSpan(
+                            child: GestureDetector(
+                              onTap: () {
+                                controller.navigateToTermsAndConditionsPage();
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 3, right: 3, bottom: 1),
+                                child: SvgPicture.asset(
+                                  assets.icTermConditionLink,
+                                  height: 10,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ]),
-                )),
-              ],
-            ),
-            Spacer(),
-            Obx(
-              () => LongButton(
-                constants.continueText,
-                enable: controller.isButtonActive.value,
-                onPressed: controller.onContinueTap,
-                isLoading: controller.isLoading.value,
+                        ]),
+                  )),
+                ],
               ),
-            ),
-          ],
+              Spacer(),
+              Obx(
+                () => LongButton(
+                  constants.continueText,
+                  enable: controller.isButtonActive.value,
+                  onPressed: controller.onContinueTap,
+                  isLoading: controller.isLoading.value,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

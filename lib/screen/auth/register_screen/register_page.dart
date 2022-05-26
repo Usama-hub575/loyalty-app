cd120650f8517ceb614ea54e63fb97c1a517d25c
$ faizanch0305 <faizanch0305@gmail.com>
2022-05-26 17:27:58 +0500

    Add button to top of keyboard

--- lib/screen/auth/register_screen/register_page.dart
+++ lib/screen/auth/register_screen/register_page.dart
@@ -12,77 +12,81 @@ class RegisterScreen extends GetView<RegisterController> {
       body: Column(children: [
         verticalSpacer(30),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+          padding:  EdgeInsets.symmetric(horizontal: horizontalValue(20), vertical: verticalValue(20)),
           child: AppBarWidget(
             title: controller.title,
             onBack: controller.onBack,
           ),
         ),
-        Expanded(
-          child: SingleChildScrollView(
-            child: Container(
-              height: sizes.height * 0.89,
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  verticalSpacer(50),
-                  Obx(
-                    () => ProfileImage(
-                      networkImage: controller.image,
-                      image: controller.imageFile.value,
-                      onTap: () => bottomSheetWidget(
-                        context,
-                        controller.pickImageCallBack,
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: horizontalValue(20)),
+          child: Expanded(
+            child: Column(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    verticalSpacer(50),
+                    Obx(
+                      () => ProfileImage(
+                        networkImage: controller.image,
+                        image: controller.imageFile.value,
+                        onTap: () => bottomSheetWidget(
+                          context,
+                          controller.pickImageCallBack,
+                        ),
                       ),
                     ),
-                  ),
-                  verticalSpacer(10),
-                  Center(
-                    child: BodySmallText(
-                      'Upload profile picture',
-                      color: colors.primaryDark,
+                    verticalSpacer(10),
+                    Center(
+                      child: BodySmallText(
+                        'Upload profile picture',
+                        color: colors.primaryDark,
+                      ),
                     ),
-                  ),
-                  verticalSpacer(30),
-                  Obx(
-                    () => AppTextField(
-                      controller.userNameController.value,
-                      hint: 'Enter your name',
-                      onTextChange: controller.onTextChange,
-                      isError: !controller.isNameValid.value,
-                      error: 'Enter valid name',
+                    verticalSpacer(30),
+                    Obx(
+                      () => AppTextField(
+                        controller.userNameController.value,
+                        hint: 'Enter your name',
+                        onTextChange: controller.onTextChange,
+                        isError: !controller.isNameValid.value,
+                        error: 'Enter valid name',
+                      ),
                     ),
-                  ),
-                  verticalSpacer(10),
-                  Obx(
-                    () => AppTextField(
-                      controller.emailController.value,
-                      hint: 'Enter your email address',
-                      onTextChange: controller.onTextChange,
-                      isError: !controller.isEmailValid.value,
-                      error: 'Enter valid email',
+                    verticalSpacer(10),
+                    Obx(
+                      () => AppTextField(
+                        controller.emailController.value,
+                        hint: 'Enter your email address',
+                        onTextChange: controller.onTextChange,
+                        isError: !controller.isEmailValid.value,
+                        error: 'Enter valid email',
+                      ),
                     ),
-                  ),
-                  verticalSpacer(10),
-                  AppTextField(
-                    controller.phoneController,
-                    readOnly: true,
-                    suffixIcon: assets.icVerified,
-                  ),
-                  Spacer(),
-                  Obx(
-                    () => LongButton(
-                      controller.buttonText,
-                      enable: controller.isEnable.value,
-                      onPressed: controller.onContinue,
-                      isLoading: controller.isLoading.value,
+                    verticalSpacer(10),
+                    AppTextField(
+                      controller.phoneController,
+                      readOnly: true,
+                      suffixIcon: assets.icVerified,
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Spacer(),
+        Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: horizontalValue(20), vertical: verticalValue(10)),
+          child: Obx(
+            () => LongButton(
+              controller.buttonText,
+              enable: controller.isEnable.value,
+              onPressed: controller.onContinue,
+              isLoading: controller.isLoading.value,
             ),
           ),
         ),

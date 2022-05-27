97dec66825bdad1a859aa2abbd137962c7ee974a
$ faizanch0305 <faizanch0305@gmail.com>
2022-05-27 18:57:03 +0500

    wip

--- lib/screen/auth/register_screen/register_page.dart
+++ lib/screen/auth/register_screen/register_page.dart
@@ -25,61 +25,59 @@ class RegisterScreen extends GetView<RegisterController> {
               ),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: horizontalValue(20)),
-                child: Expanded(
-                  child: Column(
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          verticalSpacer(50),
-                          Obx(
-                                () => ProfileImage(
-                              networkImage: controller.image,
-                              image: controller.imageFile.value,
-                              onTap: () => bottomSheetWidget(
-                                context,
-                                controller.pickImageCallBack,
-                              ),
+                child: Column(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        verticalSpacer(50),
+                        Obx(
+                              () => ProfileImage(
+                            networkImage: controller.image,
+                            image: controller.imageFile.value,
+                            onTap: () => bottomSheetWidget(
+                              context,
+                              controller.pickImageCallBack,
                             ),
                           ),
-                          verticalSpacer(10),
-                          Center(
-                            child: BodySmallText(
-                              'Upload profile picture',
-                              color: colors.primaryDark,
-                            ),
+                        ),
+                        verticalSpacer(10),
+                        Center(
+                          child: BodySmallText(
+                            'Upload profile picture',
+                            color: colors.primaryDark,
                           ),
-                          verticalSpacer(30),
-                          Obx(
-                                () => AppTextField(
-                              controller.userNameController.value,
-                              hint: 'Enter your name',
-                              onTextChange: controller.onTextChange,
-                              isError: !controller.isNameValid.value,
-                              error: 'Enter valid name',
-                            ),
+                        ),
+                        verticalSpacer(30),
+                        Obx(
+                              () => AppTextField(
+                            controller.userNameController.value,
+                            hint: 'Enter your name',
+                            onTextChange: controller.onTextChange,
+                            isError: !controller.isNameValid.value,
+                            error: 'Enter valid name',
                           ),
-                          verticalSpacer(10),
-                          Obx(
-                                () => AppTextField(
-                              controller.emailController.value,
-                              hint: 'Enter your email address',
-                              onTextChange: controller.onTextChange,
-                              isError: !controller.isEmailValid.value,
-                              error: 'Enter valid email',
-                            ),
+                        ),
+                        verticalSpacer(10),
+                        Obx(
+                              () => AppTextField(
+                            controller.emailController.value,
+                            hint: 'Enter your email address',
+                            onTextChange: controller.onTextChange,
+                            isError: !controller.isEmailValid.value,
+                            error: 'Enter valid email',
                           ),
-                          verticalSpacer(10),
-                          AppTextField(
-                            controller.phoneController,
-                            readOnly: true,
-                            suffixIcon: assets.icVerified,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                        verticalSpacer(10),
+                        AppTextField(
+                          controller.phoneController,
+                          readOnly: true,
+                          suffixIcon: assets.icVerified,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
               Spacer(),

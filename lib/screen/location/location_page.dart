cf235a678184b6b2dcaab29df0bab5ec575a67aa
$ faizanch0305 <faizanch0305@gmail.com>
2022-05-27 11:37:37 +0500

    Handling input text field hover effect

--- lib/screen/location/location_page.dart
+++ lib/screen/location/location_page.dart
@@ -5,69 +5,72 @@ import 'package:flutter_svg/svg.dart';
 class LocationPage extends GetView<LocationController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Obx(
-            () => GoogleMap(
-              mapType: MapType.normal,
-              initialCameraPosition: controller.cameraPosition.value,
-              myLocationButtonEnabled: true,
-              myLocationEnabled: true,
-              zoomControlsEnabled: false,
-              circles: controller.circle,
-              onMapCreated: (GoogleMapController mapController) {
-                controller.getUserLocation();
-                controller.completer.complete(mapController);
-                mapController.animateCamera(controller.cameraUpdate.value);
+    return GestureDetector(
+      onTap: ()=> FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Obx(
+              () => GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition: controller.cameraPosition.value,
+                myLocationButtonEnabled: true,
+                myLocationEnabled: true,
+                zoomControlsEnabled: false,
+                circles: controller.circle,
+                onMapCreated: (GoogleMapController mapController) {
+                  controller.getUserLocation();
+                  controller.completer.complete(mapController);
+                  mapController.animateCamera(controller.cameraUpdate.value);
 
-                print('=>map_created');
-              },
-              onCameraMove: (position) {
-                controller.setCameraPosition(position);
-              },
-              onCameraIdle: () {
-                controller.setGeoAddress();
-              },
-            ),
-          ),
-          Column(
-            children: [
-              Container(
-                width: sizes.width,
-                height: 40,
-                color: colors.primaryLight,
+                  print('=>map_created');
+                },
+                onCameraMove: (position) {
+                  controller.setCameraPosition(position);
+                },
+                onCameraIdle: () {
+                  controller.setGeoAddress();
+                },
               ),
-              Obx(
-                () => SearchField(
-                  controller.locationController.value,
-                  onBack: controller.onBack,
-                  onTap: controller.onTap,
-                  isReadOnly: true,
+            ),
+            Column(
+              children: [
+                Container(
+                  width: sizes.width,
+                  height: 40,
+                  color: colors.primaryLight,
+                ),
+                Obx(
+                  () => SearchField(
+                    controller.locationController.value,
+                    onBack: controller.onBack,
+                    onTap: controller.onTap,
+                    isReadOnly: true,
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 20,
+              right: 20,
+              left: 20,
+              child: Obx(
+                () => LongButton(
+                  'Confirm',
+                  onPressed: controller.onConfirm,
+                  isLoading: controller.isLoading.value,
                 ),
               ),
-            ],
-          ),
-          Positioned(
-            bottom: 20,
-            right: 20,
-            left: 20,
-            child: Obx(
-              () => LongButton(
-                'Confirm',
-                onPressed: controller.onConfirm,
-                isLoading: controller.isLoading.value,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: SvgPicture.asset(
+                assets.icDropPin,
+                color: colors.accentPrimary,
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: SvgPicture.asset(
-              assets.icDropPin,
-              color: colors.accentPrimary,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
